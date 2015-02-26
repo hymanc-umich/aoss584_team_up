7239bf78528ca276997dbe8842bb5dc93e784603
Cody Hyman <hymanc@umich.edu>
2015-02-26 16:16:19 -0500

    Committing recent changes

---
+++ FinalCode/FTU/rtftu.c
@@ -0,0 +1,26 @@
+#include <stdint.h>
+
+#include <avr/io.h>
+#include <util/interrupt.h>
+#include <util/delay.h>
+
+/**
+ * 
+ */
+int8_t initializeRTC(void)
+{
+    
+}
+
+/**
+ * 
+ */
+int main(void)
+{
+  initializeRTC();
+  while(true)
+  {
+      
+  }
+  return 0;  
+}
